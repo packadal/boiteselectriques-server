930456a053fe87207782c7e3cf3d7ebf7e3497f1
carlou <contact@andron.fr>
2016-07-20 13:43:00 +0200

    PlayThread.cpp refactoring

--- src/PlayThread.cpp
+++ src/PlayThread.cpp
@@ -1,8 +1,6 @@
 #include "PlayThread.h"
 #include "Track.h"
 
-//#define CHANNEL_INDEX channels.indexOf(qobject_cast<ChannelWidget*>(QObject::sender()))
-
 #include <io/SfxInputProxy.h>
 #include <stream_io/RtAudioOutput.h>
 #include <RtAudio.h>
@@ -10,7 +8,6 @@
 
 PlayThread::PlayThread() :
     QThread(0) {
-    qDebug() << "PlayThread";
 }
 
 unsigned int PlayThread::getTracksCount() const {
@@ -22,10 +19,10 @@ Track* PlayThread::getTrack(const unsigned int track){
 }
 
 int PlayThread::getActivatedTracks() const{
-    int res= 0;
+    int res = 0;
     for(int i= 0; i<tracks.size(); i++)
-        if(tracks[i]->isActivated())
-            res+= pow(2,i);
+        res += (tracks[i]->isActivated() ? pow(2,i) : 0);
+
     return res;
 }
 
@@ -40,28 +37,26 @@ void PlayThread::run() {
     manager->execute();
 }
 
-void PlayThread::setMasterVolume(const int vol) {
+void PlayThread::setMasterVolume(const unsigned int vol) {
     masterVolume->setGain(vol / 10.0);
 }
 
-void PlayThread::setVolume(const int track, const int vol) {
-    //volumes[track]->setGain(vol / 100.0);
+void PlayThread::setVolume(const unsigned int track, const unsigned int vol) {
     tracks[track]->setVolume(vol);
 }
 
-void PlayThread::setPan(const int track, const int pan) {
-    //pans[track]->setPan(pan / 100.0);
+void PlayThread::setPan(const unsigned int track, const int pan) {
     tracks[track]->setPan(pan);
 }
 
-void PlayThread::setMute(const int track, const bool doMute) {
-    //doMute? mutes[track]->mute() : mutes[track]->unmute();
-    qDebug() << "THREAD::SET_MUTE" << track << (doMute ? "TRUE" : "FALSE");
+void PlayThread::setMute(const unsigned int track, const bool doMute) {
     tracks[track]->setMute(doMute);
 }
 
 void PlayThread::timeHandle() {
-    if(++bufferCount > maxBufferCount) bufferCount = 0;
+    if(++bufferCount > maxBufferCount)
+        bufferCount = 0;
+
     emit actualBeatChanged(bufferCount * conf.bufferSize / double(conf.samplingRate));
 }
 
@@ -78,55 +73,41 @@ bool PlayThread::isStopped() {
 
 void PlayThread::reset() {
     setMasterVolume(DEFAULT_MASTER_VOLUME);
+
     for(int i=0; i<tracks.size(); i++)
         tracks[i]->reset();
 }
 
-void PlayThread::solo(const int track, const bool state) {
-    qDebug() << "PLAYTHREAD::SOLO" << track << (state ? "TRUE" : "FALSE");
+void PlayThread::solo(const unsigned int track, const bool state) {
     if(track < tracks.size())
         tracks[track]->setSolo(state);
 
     if(state) {
         tracks[track]->setMute(false);
 
-        qDebug() << "TO_MUTE:";
         for(int i=0; i<tracks.size(); i++)
-            if(!tracks[i]->isSolo()){
-                qDebug() << "MUTE->" << i;
+            if(!tracks[i]->isSolo())
                 tracks[i]->setMute(true);
-            }
     } else {
-        qDebug() << "UNMUTE ?";
-
         bool noMoreSolo = true;
         for(int i=0; i<tracks.size() && noMoreSolo; i++)
             if(tracks[i]->isSolo())
                 noMoreSolo = false;
 
         if(noMoreSolo)
-        {
-            qDebug() << "UNMUTE :";
             for(int i=0; i<tracks.size(); i++)
-            {
-                qDebug() << "UNMUTE->" << i;
                 tracks[i]->setMute( !tracks[i]->isActivated() );
-            }
-        } else
+        else
             tracks[track]->setMute( true );
     }
 }
 
 void PlayThread::switchBox(const unsigned int track) {
-    qDebug() << (tracks[track]->isActivated() ? "DES" : "") << "ACTIVATE";
     tracks[track]->setActivated(!tracks[track]->isActivated());
-    qDebug() << "SWITCHED";
 }
 
 void PlayThread::setThreshold(const int threshold){
-    qDebug() << "THREAD::SET_THRESHOLD" << threshold;
     m_threshold = (99 -  threshold) * 4 + 100;
-    qDebug() << "NEW THRESHOLD" << m_threshold;
 }
 
 void PlayThread::resetThreshold() {
@@ -134,47 +115,36 @@ void PlayThread::resetThreshold() {
 }
 
 void PlayThread::load(const SongData& s) {
-    //// Remise à 0
+    // Reset to 0
     bufferCount = 0;
     int track_count = s.tracks.size();
-    /*volumes.clear();
-    pans.clear();
-    mutes.clear();*/
     for(int i=0; i<tracks.size(); i++)
         delete tracks[i];
     tracks.clear();
-
     manager.reset();
 
-    /*volumes.resize(track_count);
-    pans.resize(track_count);
-    mutes.resize(track_count);*/
+    // Loading
     tracks.resize(track_count);
-
-    //// Chargement
     std::vector<Input_p> chains(track_count);
 
 #pragma omp parallel for
     for(int i = 0; i < track_count; i++) {
-        /*volumes[i] = std::make_shared<Amplify<double>>(conf);
-        pans[i] = std::make_shared<Pan<double>>(conf);
-        mutes[i] = std::make_shared<Mute<double>>(conf);*/
-
         Track* t = new Track(s.tracks[i], conf, i);
-        //connect(channels.last(), SIGNAL(on_enable(bool)), this, SLOT(on_enablementChanged(bool)));
-        connect(t, &Track::on_enable, this, &PlayThread::onEnablementChanged);
+        connect(t, &Track::on_enable,
+                this, &PlayThread::onEnablementChanged);
         tracks[i] = t;
 
-
-        auto file = new FFMPEGFileInput<double>(/*s.tracks[i].file*/tracks[i]->getFile(), conf);
+        auto file = new FFMPEGFileInput<double>(tracks[i]->getFile(), conf);
         maxBufferCount = file->v(0).size() / conf.bufferSize;
         emit beatCountChanged(file->v(0).size() / double(conf.samplingRate));
+
         chains[i] = Input_p(new SfxInputProxy<double>(new StereoAdapter<double>(new LoopInputProxy<double>(file)),
-                            new Sequence<double>(conf, tracks[i]->getVolumePtr(), tracks[i]->getPanPtr(), tracks[i]->isMute() /*volumes[i], pans[i], mutes[i]*/)));
+                            new Sequence<double>(conf, tracks[i]->getVolumePtr(), tracks[i]->getPanPtr(), tracks[i]->isMute() )));
+
         emit songLoaded(i+1,track_count);
     }
 
-    // Piste master
+    // Master
     auto input = Input_p(new SfxInputProxy<double>(new SummationProxy<double>(new InputMultiplexer<double>(conf, chains)), masterVolume));
 
     // Manager
@@ -185,10 +155,6 @@ void PlayThread::load(const SongData& s) {
 }
 
 void PlayThread::onEnablementChanged(bool enabled, int track) {
-    qDebug() << "enablementChanged";
-    qDebug() << "emit MUTE_CHANGED";
-             /*<< ( enabled ? "TRUE" : "FALSE" )
-             << box;*/
     emit muteChanged(track,
                      !enabled);
 }
